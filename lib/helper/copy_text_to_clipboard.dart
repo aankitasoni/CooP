e6add847142af72b352e81279848f7150a676dc2
Ankita Soni <ankitasoni018@gmail.com>
2024-02-08 17:21:29 +0530

    clipboard updated

--- lib/helper/copy_text_to_clipboard.dart
+++ lib/helper/copy_text_to_clipboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 
 import 'my_dialog.dart';
 
+
 void copyToClipboard(String text) {
   Clipboard.setData(ClipboardData(text: text));
   MyDialog.success('Message Copied to Clipboard');
